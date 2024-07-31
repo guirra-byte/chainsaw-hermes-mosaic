db66b8b9fcd4e9dece9bf2dcc014330576a46db5
guirra-byte <guirramatheus1@gmail.com>
2024-07-31 11:21:36 -0300

    refactor: update trim time range format

--- src/helpers/seconds-to-time.ts
+++ src/helpers/seconds-to-time.ts
@@ -1,13 +1,6 @@
-function secondsToTime(seconds: number) {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const remainingSeconds = seconds % 60;
-
-  const reply = `${String(hours).padStart(2, "0")}
-  :${String(minutes).padStart(2, "0")}
-  :${String(remainingSeconds).padStart(2, "0")}`;
-
-  return reply;
+function secondsToTime(time: string) {
+  const timeRegex = /^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
+  return timeRegex.test(time);
 }
 
 export { secondsToTime };
